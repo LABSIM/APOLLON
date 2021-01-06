ddb543c325353692c32a9d492faaa73becaefb36
nkinani <nawfel.kinani@onera.fr>
2021-01-06 16:38:22 +0100

    [dev]
    - add result variable
    - correct handling of catch try condition
    - correct handling of pos/neg stim
    - validate passive condition

--- Assets/Engine/Apollon/experiment/profile/ApollonAgencyAndThresholdPerceptionProfile.cs
+++ Assets/Engine/Apollon/experiment/profile/ApollonAgencyAndThresholdPerceptionProfile.cs
@@ -66,9 +66,11 @@ namespace Labsim.apollon.experiment.profile
                 user_command;
 
             public float
+                user_stim_unity_timestamp,
                 user_perception_unity_timestamp;
 
             public string
+                user_stim_host_timestamp,
                 user_perception_host_timestamp;
 
             public UnityEngine.AudioClip
@@ -345,87 +347,102 @@ namespace Labsim.apollon.experiment.profile
                 UnityEngine.Debug.Log(
                     "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : begin"
                 );
+                
 
-                // if not a try/catch condition 
-                if (!this.FSM.CurrentSettings.bIsTryCatch)
-                //if (this.FSM.CurrentSettings.bIsActive && !this.FSM.CurrentSettings.bIsTryCatch)
+                // get bridges
+                gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorBridge hotas_bridge
+                    = gameplay.ApollonGameplayManager.Instance.getBridge(
+                        gameplay.ApollonGameplayManager.GameplayIDType.HOTASWarthogThrottleSensor
+                    ) as gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorBridge;
+
+                gameplay.entity.ApollonActiveSeatEntityBridge seat_bridge
+                    = gameplay.ApollonGameplayManager.Instance.getBridge(
+                        gameplay.ApollonGameplayManager.GameplayIDType.ActiveSeatEntity
+                    ) as gameplay.entity.ApollonActiveSeatEntityBridge;
+
+                // check
+                if (hotas_bridge == null || seat_bridge == null)
                 {
 
-                    // get bridges
-                    gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorBridge hotas_bridge
-                        = gameplay.ApollonGameplayManager.Instance.getBridge(
-                            gameplay.ApollonGameplayManager.GameplayIDType.HOTASWarthogThrottleSensor
-                        ) as gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorBridge;
-
-                    gameplay.entity.ApollonActiveSeatEntityBridge seat_bridge
-                        = gameplay.ApollonGameplayManager.Instance.getBridge(
-                            gameplay.ApollonGameplayManager.GameplayIDType.ActiveSeatEntity
-                        ) as gameplay.entity.ApollonActiveSeatEntityBridge;
-
-                    // check
-                    if (hotas_bridge == null || seat_bridge == null)
-                    {
-
-                        // log
-                        UnityEngine.Debug.LogError(
-                            "<color=Red>Error: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : Could not find corresponding gameplay bridge !"
-                        );
-
-                        // fail
-                        return;
-
-                    } /* if() */
-
-                    // synchronisation mechanism (TCS + local function)
-                    TaskCompletionSource<(bool, float, string)> sync_point = new TaskCompletionSource<(bool, float, string)>();
-                    void sync_user_response_local_function(object sender, gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorDispatcher.EventArgs e)
-                        => sync_point?.TrySetResult((true, UnityEngine.Time.time, System.DateTime.Now.ToString("HH:mm:ss.ffffff")));
-                    void sync_end_stim_local_function(object sender, gameplay.entity.ApollonActiveSeatEntityDispatcher.EventArgs e)
-                        => sync_point?.TrySetResult((false, -1.0f, "-1"));
-
-                    // register our synchronisation function
-                    hotas_bridge.Dispatcher.UserResponseTriggeredEvent += sync_user_response_local_function;
-                    seat_bridge.Dispatcher.StopEvent += sync_end_stim_local_function;
-
-                    UnityEngine.Debug.Log(
-                        "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : begin stim"
+                    // log
+                    UnityEngine.Debug.LogError(
+                        "<color=Red>Error: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : Could not find corresponding gameplay bridge !"
                     );
 
-                    // begin stim
-                    seat_bridge.Dispatcher.RaiseAccelerate(
-                        this.FSM.CurrentResults.user_command * System.Math.Abs(this.FSM.CurrentSettings.phase_C_angular_acceleration),
-                        this.FSM.CurrentResults.user_command * System.Math.Abs(this.FSM.CurrentSettings.phase_C_angular_saturation_speed),
-                        this.FSM.CurrentSettings.phase_C_max_stim_duration,
-                        this.FSM.CurrentSettings.phase_C_max_stim_angle,
-                        (this.FSM.CurrentSettings.scenario_type == Settings.ScenarioIDType.VisualOnly ? true : false)
-                    );
-
-                    UnityEngine.Debug.Log(
-                        "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : wait end stim"
-                    );
-
-                    // wait synchronisation point indefinitely & reset it once hit
-                    (
-                        this.FSM.CurrentResults.user_response, 
-                        this.FSM.CurrentResults.user_perception_unity_timestamp,
-                        this.FSM.CurrentResults.user_perception_host_timestamp
-                    ) = await sync_point.Task;
-
-                    UnityEngine.Debug.Log(
-                        "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : end stim, result [user_response:"
-                        + this.FSM.CurrentResults.user_response
-                        + ",user_perception_unity_timestamp:"
-                        + this.FSM.CurrentResults.user_perception_unity_timestamp
-                        + ",user_perception_host_timestamp:"
-                        + this.FSM.CurrentResults.user_perception_host_timestamp
-                        + "]"
-                    );
-
-                    // unregister our synchronisation function
-                    hotas_bridge.Dispatcher.UserResponseTriggeredEvent -= sync_user_response_local_function;
-                    seat_bridge.Dispatcher.StopEvent -= sync_end_stim_local_function;
+                    // fail
+                    return;
 
                 } /* if() */
+
+                // synchronisation mechanism (TCS + local function)
+                TaskCompletionSource<(bool, float, string)> sync_point = new TaskCompletionSource<(bool, float, string)>();
+                void sync_user_response_local_function(object sender, gameplay.device.sensor.ApollonHOTASWarthogThrottleSensorDispatcher.EventArgs e)
+                    => sync_point?.TrySetResult((true, UnityEngine.Time.time, System.DateTime.Now.ToString("HH:mm:ss.ffffff")));
+                void sync_end_stim_local_function(object sender, gameplay.entity.ApollonActiveSeatEntityDispatcher.EventArgs e)
+                    => sync_point?.TrySetResult((false, -1.0f, "-1"));
+
+                // register our synchronisation function
+                hotas_bridge.Dispatcher.UserResponseTriggeredEvent += sync_user_response_local_function;
+                seat_bridge.Dispatcher.StopEvent += sync_end_stim_local_function;
+
+                UnityEngine.Debug.Log(
+                    "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : begin stim"
+                );
+
+                // log stim begin timestamp
+                this.FSM.CurrentResults.user_stim_unity_timestamp = UnityEngine.Time.time;
+                this.FSM.CurrentResults.user_stim_host_timestamp = System.DateTime.Now.ToString("HH:mm:ss.ffffff");
+                
+                // begin stim
+                seat_bridge.Dispatcher.RaiseAccelerate(
+                    /* if not a try/catch condition ? */ (
+                        this.FSM.CurrentSettings.bIsTryCatch
+                            ? 0.0f
+                            : (this.FSM.CurrentResults.user_command * this.FSM.CurrentSettings.phase_C_angular_acceleration)
+                    ),
+                    /* if not a try/catch condition ? */ (
+                        this.FSM.CurrentSettings.bIsTryCatch
+                            ? 0.0f
+                            : (this.FSM.CurrentResults.user_command * this.FSM.CurrentSettings.phase_C_angular_saturation_speed)
+                    ),
+                    this.FSM.CurrentSettings.phase_C_max_stim_duration,
+                    this.FSM.CurrentSettings.phase_C_max_stim_angle,
+                    (this.FSM.CurrentSettings.scenario_type == Settings.ScenarioIDType.VisualOnly ? true : false)
+                );
+                
+                // log
+                UnityEngine.Debug.Log(
+                    "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : begin "
+                    + (this.FSM.CurrentSettings.bIsTryCatch ? "fake" : "real")
+                    + " stim, result [user_stim_unity_timestamp:"
+                    + this.FSM.CurrentResults.user_stim_unity_timestamp
+                    + ",user_stim_host_timestamp:"
+                    + this.FSM.CurrentResults.user_stim_host_timestamp
+                    + "]"
+                );
+
+                // wait synchronisation point indefinitely & reset it once hit
+                (
+                    this.FSM.CurrentResults.user_response, 
+                    this.FSM.CurrentResults.user_perception_unity_timestamp,
+                    this.FSM.CurrentResults.user_perception_host_timestamp
+                ) = await sync_point.Task;
+
+                // log
+                UnityEngine.Debug.Log(
+                    "<color=Blue>Info: </color> ApollonAgencyAndThresholdPerceptionProfile.PhaseC.OnEntry() : end stim, result [user_response:"
+                    + this.FSM.CurrentResults.user_response
+                    + ",user_perception_unity_timestamp:"
+                    + this.FSM.CurrentResults.user_perception_unity_timestamp
+                    + ",user_perception_host_timestamp:"
+                    + this.FSM.CurrentResults.user_perception_host_timestamp
+                    + "]"
+                );
+
+                // unregister our synchronisation function
+                hotas_bridge.Dispatcher.UserResponseTriggeredEvent -= sync_user_response_local_function;
+                seat_bridge.Dispatcher.StopEvent -= sync_end_stim_local_function;
+
 
                 // log
                 UnityEngine.Debug.Log(
@@ -756,6 +773,8 @@ namespace Labsim.apollon.experiment.profile
             //);
 
             // write result
+            ApollonExperimentManager.Instance.Trial.result["user_stim_host_timestamp"] = this.CurrentResults.user_stim_host_timestamp;
+            ApollonExperimentManager.Instance.Trial.result["user_stim_unity_timestamp"] = this.CurrentResults.user_stim_unity_timestamp;
             ApollonExperimentManager.Instance.Trial.result["user_response"] = this.CurrentResults.user_response;
             ApollonExperimentManager.Instance.Trial.result["user_perception_host_timestamp"] = this.CurrentResults.user_perception_host_timestamp;
             ApollonExperimentManager.Instance.Trial.result["user_perception_unity_timestamp"] = this.CurrentResults.user_perception_unity_timestamp;
